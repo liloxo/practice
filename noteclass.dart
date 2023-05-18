9f5166f584f95d7be3329a801fd81d4309ce3f0c
Mohamed Rahal <110819069+liloxo@users.noreply.github.com>
2023-05-18 20:47:22 +0200

    Update noteclass.dart

--- noteclass.dart
+++ noteclass.dart
@@ -10,18 +10,42 @@ class TaskData {
   List notes = [];
 
   getdata() {
+    // retrieve the stored note data from SharedPreferences
     List<String>? response = sharedPreferences.getStringList('note');
+    // check if the response is not null 
     if (response != null) {
-      for (String noteData in response) {
-        List<String> noteDetails = noteData.split(',');
-        notes.add(
-          Note(
-            title: noteDetails[0],
-            content: noteDetails.length > 1 ? noteDetails[1] : null,
-          ),
-        );
-      }
+      // iterate over each string in the response list. In each iteration, the current string is assigned to the noteData variable
+    notes = response.map((noteData) {
+      // title,content
+      List<String> noteDetails = noteData.split(',');
+      // [title,content]
+      return Note(
+        title: noteDetails[0],
+        content: noteDetails.length > 1 ? noteDetails[1] : null,
+      );
+    }).toList();
+    notes = notes.reversed.toList(); // Reverse the list to display in reverse order
     }
+  }
+
+  void updateNoteByTitle(String title, String newTitle, String? newContent) {
+  for (int i = 0; i < notes.length; i++) {
+    if (notes[i].title == title) {
+      notes[i].title = newTitle;
+      notes[i].content = newContent;
+      break;
+    }
+  }
+}
+
+  deletenote(String ttl) async {
+    notes.removeWhere((note) => note.title == ttl);
+    saveNotesToSharedPrefs();
+  }
+
+  Future<void> saveNotesToSharedPrefs() async {
+    List<String> notesData = notes.map((note) => "${note.title},${note.content}").toList();
+    await sharedPreferences.setStringList('note', notesData);
   }
 }
 
